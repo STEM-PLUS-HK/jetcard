2399ba3207e07eb04bd28405d49ebec2c628bc7a
StemPlusLeslie <127295527+StemPlusLeslie@users.noreply.github.com>
2023-11-08 21:16:01 +0800

    update jetcard reuqirement

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='Easily make projects with NVIDIA Jetson Nano',
     packages=find_packages(),
     install_requires=[
-        'Adafruit_SSD1306'
+        'Adafruit_SSD1306',
+        'Jetson.GPIO'
     ],
 )
