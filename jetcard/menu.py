22390d16b8f7f586ce1271ace3e2c5fa0ff1f900
StemPlusLeslie <127295527+StemPlusLeslie@users.noreply.github.com>
2024-01-23 14:37:14 +0800

    Update menu.py

--- jetcard/menu.py
+++ jetcard/menu.py
@@ -44,11 +44,17 @@ class OLEDMenu:
         arg = {'root': obj.root.uuid if obj.root else 'base',
                'name': obj.get_description(),
                'uuid': obj.uuid}
-        if isinstance(obj, Menu):
+        if isinstance(obj, Function):
+            self.send({'action': 'create_func', 'arg': arg})
+        elif isinstance(obj, Menu):
             self.send({'action': 'create_menu', 'arg': arg})
-        else:
+        elif isinstance(obj, Variable):
             arg['value'] = obj.get_value()
             arg['step'] = obj.get_step()
+            self.send({'action': 'create_var', 'arg': arg})
+        else:
+            arg['value'] = None
+            arg['step'] = None
             self.send({'action': 'create_var', 'arg': arg})
         self.obj_list.append(obj)
         
@@ -58,8 +64,8 @@ def reset_menu():
     global oled_menu
     oled_menu.reset()
 
-class Menu:
-    def __init__(self, root=None, description=""):
+class Item:
+    def __init__(self, *args, root=None, description="", **kwargs):
         global oled_menu
         self.root = root
         self._description = description
@@ -69,15 +75,49 @@ class Menu:
     def get_description(self):
         return self._description
     
-class Variable:
-    def __init__(self, root=None, value=None, step=None, description=None, *args, **kwargs):
+class Menu(Item):
+    def __init__(self, *args, root=None, description="", **kwargs):
+        super().__init__(*args, root=root, description=description, **kwargs)
+        
+    def reset(self):
         global oled_menu
-        self.root = root
-        self.uuid = str(uuid.uuid4())
+        if hasattr(self, 'uuid'):
+            packet = {'action':'reset_menu', 'uuid':self.uuid}
+            oled_menu.send(packet)
+    
+class Function(Menu):
+    def __init__(self, callback_func, *args, root=None, description="", **kwargs):
+        '''
+        Callback argument: callback_func(self)
+        Callback return: if return is True, the OLED menu will go back to the main menu immediately
+        '''
+        self.callback = callback_func
+        self.callback_thread = None
+        super().__init__(*args, root=root, description=description, **kwargs)
+        
+    # used by OLEDMenu class only
+    def update(self, value):
+        if self.callback_thread != None:
+            self.callback_thread.join()
+        self.callback_thread = threading.Thread(target=self.callback_wrapper)
+        self.callback_thread.start()
+        
+    def callback_wrapper(self):
+        global oled_menu
+        ret = True if self.callback == None else self.callback(self)
+        if hasattr(self, 'uuid'):
+            packet = {'action':'value_update', 'uuid':self.uuid, 'value':ret==True}
+            oled_menu.send(packet)
+            
+    def callback_print(self, output):
+        print_data = str(output)
+        item = Item(root=self, description=print_data)
+    
+class Variable(Item):
+    def __init__(self, *args, root=None, value=None, step=None, description=None, **kwargs):
         self._value = value
         self._step = step
-        self._description = description
-        oled_menu.add(self)
+        super().__init__(*args, root=root, description=description, **kwargs)
         
     # used by OLEDMenu class only
     def update(self,value):
@@ -95,30 +135,26 @@ class Variable:
         
     def get_step(self):
         return self._step
-    
-    def get_description(self):
-        return self._description
         
 class FloatVariable(Variable):
-    def __init__(self, root=None, value=0.0, step=0.1, description='', *args, **kwargs):
-        print(value, step, description, args, kwargs)
-        super().__init__(root=root, value=float(value), step=step, description=description, *args, **kwargs)
+    def __init__(self, *args, root=None, value=0.0, step=0.1, description='', **kwargs):
+        super().__init__(*args, root=root, value=float(value), step=step, description=description, **kwargs)
         
     # used by OLEDMenu class only
     def update(self, value):
         self._value = float(value)
             
 class IntVariable(Variable):    
-    def __init__(self, root=None, value=0, step=1, description='', *args, **kwargs):
-        Variable.__init__(self, root=root, value=int(value), step=step, description=description, *args, **kwargs)
+    def __init__(self, *args, root=None, value=0, step=1, description='', **kwargs):
+        super().__init__(*args, root=root, value=int(value), step=step, description=description, **kwargs)
         
     # used by OLEDMenu class only
     def update(self, value):
         self._value = int(value)
 
 class BoolVariable(Variable):
-    def __init__(self, root=None, value=True, description='', *args, **kwargs):
-        Variable.__init__(self, root=root, value=bool(value), step=None, description=description, *args, **kwargs)
+    def __init__(self, *args, root=None, value=True, description='', **kwargs):
+        super().__init__(*args, root=root, value=bool(value), step=None, description=description, **kwargs)
         
     # used by OLEDMenu class only
     def update(self, value):
